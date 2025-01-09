4ab0620a83a82091aea8e87d98700e21a86aa1c6
sophondetector <nateisgood@gmail.com>
2025-01-08 19:03:10 -0500

    change filepath in options.ts

--- src/options.ts
+++ src/options.ts
@@ -1,5 +1,5 @@
 document.querySelector('#reeder-button')!.addEventListener('click', () => {
 	console.log("opening Legis app from tray button...")
-	window.open('../pages/index.html')
+	window.open('/pages/index.html')
 })
 
