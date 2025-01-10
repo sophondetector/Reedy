2483bad36d5d3afe3e3f67124ba33a1ee35db020
sophondetector <nateisgood@gmail.com>
2025-01-10 11:00:29 -0500

    add site handler injection stubs

--- src/content.ts
+++ src/content.ts
@@ -9,3 +9,29 @@ chrome.runtime.onMessage.addListener(function(response, sender, sendResponse) {
 		sendResponse({ err })
 	}
 })
+
+function getCurrentDomain(): string {
+	return window.location.host.match(/\w+\.\w+$/)!.toString()
+}
+
+function vaticanHandler(): void {
+	console.log("Legis vatican handler!")
+}
+
+function wikiHandler(): void {
+	console.log("Legis wiki handler!")
+}
+
+const DOMAIN_HANDLER_MAP = new Map()
+DOMAIN_HANDLER_MAP.set("vatican.va", vaticanHandler)
+DOMAIN_HANDLER_MAP.set("wikipedia.org", wikiHandler)
+
+const SUPPORTED_DOMAINS = Array.from(DOMAIN_HANDLER_MAP.keys())
+const CURRENT_DOMAIN = getCurrentDomain()
+
+if (SUPPORTED_DOMAINS.includes(CURRENT_DOMAIN)) {
+	DOMAIN_HANDLER_MAP.get(CURRENT_DOMAIN)()
+} else {
+	console.log(`Legis does not support ${CURRENT_DOMAIN}`)
+}
+
