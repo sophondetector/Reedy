cecc3d96e2987e39a771a407b7bdf20a471e0203
sophondetector <nateisgood@gmail.com>
2025-01-15 16:17:12 -0500

    simplify move everything but public/images into repo root to simplify paths; add paths to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.json'
 
 export default defineConfig({
+	build: {
+		rollupOptions: {
+			input: {
+				app: "pages/index.html",
+				pdfReader: "pages/pdf-reader.html"
+			}
+		}
+	},
 	plugins: [crx({ manifest })]
 })
