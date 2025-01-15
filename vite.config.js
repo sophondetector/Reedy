74e73482630e3a81f033e8691b92ea84cd754dad
sophondetector <nateisgood@gmail.com>
2025-01-15 12:55:32 -0500

    remove command from vite that wasn't doing anything

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.json'
 
 export default defineConfig({
-	build: {
-		rollupOptions: {
-			input: {
-				main: resolve(__dirname, 'public/pages/index.html'),
-			},
-		},
-	},
 	plugins: [crx({ manifest })]
 })
