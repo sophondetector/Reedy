79fd4d98823d9d7034398378a7ac9c9272bca0f4
sophondetector <nateisgood@gmail.com>
2025-01-22 18:14:31 -0500

    half way working but ranges get destroyed

--- src/line-by-line.ts
+++ src/line-by-line.ts
@@ -1,17 +1,68 @@
 import { LECS } from "./consts"
 
 const TEXT_NODE_NAME = '#text'
-let CACHED_PARA: Node | null = null
 
-function setCachedPara(para: Node): void {
-	CACHED_PARA = para.cloneNode(true)
+let RANGES: Range[] | null = null
+let RANGE_IDX: number = 0
+let MAX_IDX: number | null = null
+let CACHED_PARAS: HTMLElement[] | null = null
+
+// ON INCREMENT/DECREMENT
+// increment range-idx
+// replace doc para with clone of cached para
+// surround ranges[rangeIdx] with <highlight></highlight>
+
+function restoreCachedPara(paraIdx: number): void {
+	if (CACHED_PARAS === null) {
+		throw new Error(`no cached paras!`)
+	}
+
+	const mangled = document.querySelector(`#para${paraIdx}`) as HTMLElement
+	const fromCache = CACHED_PARAS[paraIdx].cloneNode(true)
+
+	if (!fromCache) {
+		throw new Error(`cached paras miss for ${paraIdx}`)
+	}
+	mangled.replaceWith(fromCache)
 }
 
-function getCachedPara(): Node {
-	if (!CACHED_PARA) {
-		throw Error('no cached para set!')
+function isPara(ele: HTMLElement | null): boolean {
+	if (ele === null) {
+		throw new Error('range2Para null parentElement!')
 	}
-	return CACHED_PARA.cloneNode()
+	return ele.classList.contains('reading-room-para')
+}
+
+function range2Para(rng: Range): HTMLElement {
+	let parent = rng.commonAncestorContainer.parentElement
+	while (!isPara(parent)) {
+		parent = parent!.parentElement
+	}
+	return parent as HTMLElement
+}
+
+function para2Idx(para: HTMLElement): number {
+	const numStr = para.id.match(/\d+/)!.toString()
+	const num = Number(numStr)
+	return num
+}
+
+function incLine(): void {
+	console.log('inc line!')
+	if (RANGES === null) return
+	if (RANGE_IDX === RANGES.length - 1) {
+		return
+	}
+
+	const oldRng = RANGES[RANGE_IDX]
+	const oldPara = range2Para(oldRng)
+	const oldParaIdx = para2Idx(oldPara)
+	restoreCachedPara(oldParaIdx)
+
+	RANGE_IDX++
+
+	const rng = RANGES[RANGE_IDX]
+	rng.surroundContents(document.createElement('target'))
 }
 
 function getAllTextNodes(node: Node): Node[] {
@@ -88,26 +139,44 @@ function para2Ranges(paraEle: Element): Array<Range> {
 	return res
 }
 
-document.querySelector("#reading-mode-switch")!.addEventListener("click", function() {
-	// const p = document.querySelector('p') as HTMLElement
-	// const rngs = para2Ranges(p)
-	// console.log(`ranges`)
-	// rngs.forEach(r => console.log(r.toString()))
-	console.log('sent-by-sent click!')
+function paras2Ranges(paras: Element[]): Range[] {
+	const res = []
+	for (const para of paras) {
+		res.push(...para2Ranges(para))
+	}
+	return res
+}
+
+function getMainParas(): HTMLElement[] {
+	const main = document.querySelector(LECS.main.mainContent)
+	const arr = Array.from(main!.children) as HTMLElement[]
+	return arr
+}
+
+//TODO this needs to happen on mainContent lec being filled
+document.querySelector("#cache-paras")!.addEventListener("click", function() {
+	CACHED_PARAS = []
+	const childParas = Array.from(document.querySelector(LECS.main.mainContent)!.children) as HTMLElement[]
+	for (const para of childParas) {
+		CACHED_PARAS.push(para.cloneNode(true) as HTMLElement)
+	}
+	console.log(`para cache done`)
 })
 
-// ON INCREMENT
-// increment range-idx
-// replace doc para with clone of cached para
-// surround ranges[rangeIdx] with <highlight></highlight>
+//TODO this needs to happen on mainContent lec being filled
+document.querySelector("#line-by-line")!.addEventListener("click", function() {
+	const paras = getMainParas()
+	RANGES = paras2Ranges(paras)
+	for (const rng of RANGES) {
+		console.log(rng.toString())
+	}
+})
+
+document.querySelector('#inc-line')!.addEventListener("click", incLine)
 
 window.onresize = () => {
 	const mainContent = document.querySelector(LECS.main.mainContent)
-	const paras = mainContent!.children
-	for (const para of paras) {
-		const ranges = para2Ranges(para)
-		for (const rng of ranges) {
-			console.log(rng.toString())
-		}
-	}
+	const paras = getMainParas()
+	RANGES = paras2Ranges(paras)
+	MAX_IDX = RANGES.length
 }
