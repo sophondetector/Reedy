abf68fb9144a4a756820426c469a3e0010e86339
sophondetector <nateisgood@gmail.com>
2025-03-04 15:35:02 -0500

    Add test block to ele2Lec

--- src/ele2Lec.ts
+++ src/ele2Lec.ts
@@ -24,3 +24,17 @@ export function ele2Lec(ele: HTMLElement): string {
 
 	return `${ele2Lec(ele.parentElement!)} > ${current}`
 }
+
+// TEST BLOCK FOR ele2Lec
+// const lecs = [
+// 	'#content > article > div > p:nth-child(4)',
+// 	'#your_first_website_creating_the_content > a',
+// 	'#content > article > section:nth-child(4) > div > dl > dd:nth-child(8) > p > a:nth-child(4) > code'
+// ]
+//
+// for (const le of lecs) {
+// 	const ele = Q(le) as HTMLElement
+// 	const outLec = ele2Lec(ele)
+// 	console.log(`outLec: ${outLec}`)
+// }
+
