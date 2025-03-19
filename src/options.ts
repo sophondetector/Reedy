b20df101adf24670ad5f9f396339ca9635c78d64
sophondetector <nateisgood@gmail.com>
2025-03-19 17:10:56 -0400

    add TODO

--- src/options.ts
+++ src/options.ts
@@ -1,3 +1,4 @@
+// TODO add slider to control transparency of visorScreen
 // TODO put controls here
 document.querySelector('#reeder-button')!.addEventListener('click', () => {
 	console.log("you clicked the button")
