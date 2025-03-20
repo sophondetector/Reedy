2cfe32ef9e5b7d92f8b65a2ecaaf4eedbc75a209
sophondetector <nateisgood@gmail.com>
2025-03-20 16:23:22 -0400

    add TODO to genericHandler

--- src/site-handlers/generic-handler.ts
+++ src/site-handlers/generic-handler.ts
@@ -1,3 +1,4 @@
+// TODO this has an issue where ranges are cut off one char too early; why?!?!?!
 export function genericHandler(): Array<Element> {
 	const qsaResult = document.querySelectorAll('h1, h2, h3, h4, h5, p')
 	const arr = Array.from(qsaResult)
