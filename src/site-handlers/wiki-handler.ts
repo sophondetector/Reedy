5fefd5d6a66e85c647738b0ce5d0fab370a948da
sophondetector <nateisgood@gmail.com>
2025-03-19 22:28:01 -0400

    add TODO to wiki handler

--- src/site-handlers/wiki-handler.ts
+++ src/site-handlers/wiki-handler.ts
@@ -1,4 +1,5 @@
 export function wikiHandler(): Element {
+	// TODO fix bug where it goes to [tools] section
 	const mainContent = document.querySelector('#mw-content-text')
 	if (!mainContent) {
 		throw new Error('wikiHandler: could not find main content!')
