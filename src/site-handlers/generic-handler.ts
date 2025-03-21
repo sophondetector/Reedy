6fc0c940dbbf8dcc316d9c1f99628fc2eccebf5d
sophondetector <nateisgood@gmail.com>
2025-03-21 16:21:49 -0400

    generic-handler function signature now takes a "root" parameter; which if null becomes the document
    idea is to be able to pass a root element to the handler which
    encompasses all the interesting content

--- src/site-handlers/generic-handler.ts
+++ src/site-handlers/generic-handler.ts
@@ -1,5 +1,9 @@
 // TODO this has an issue where ranges are cut off one char too early; why?!?!?!
-export function genericHandler(): Array<Element> {
+export function genericHandler(root: Document | Element | null = null): Array<Element> {
+	if (root === null) {
+		root = document
+	}
+
 	const qsaResult = document.querySelectorAll('h1, h2, h3, h4, h5, p')
 	const arr = Array.from(qsaResult)
 	return arr
