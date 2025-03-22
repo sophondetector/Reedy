7ae811126c363c89432dd544b90ce37349fe210f
sophondetector <nateisgood@gmail.com>
2025-03-21 20:07:56 -0400

    Make wiki handler work again

--- src/site-handlers/index.ts
+++ src/site-handlers/index.ts
@@ -2,7 +2,7 @@ import { genericHandler } from "./generic-handler"
 // import { mdnHandler } from "./mdn-handler"
 // import { substackHandler } from "./substack-handler"
 // import { vaticanHandler } from "./vatican-handler"
-// import { wikiHandler } from "./wiki-handler"
+import { wikiHandler } from "./wiki-handler"
 
 // TODO refactor so handlers return ranges
 export const GENERIC_HANDLER_KEY: string = "GENERIC"
@@ -11,7 +11,7 @@ export const DOMAIN_HANDLER_MAP = new Map()
 
 DOMAIN_HANDLER_MAP.set(GENERIC_HANDLER_KEY, genericHandler)
 DOMAIN_HANDLER_MAP.set("vatican.va", genericHandler)
-DOMAIN_HANDLER_MAP.set("wikipedia.org", genericHandler)
+DOMAIN_HANDLER_MAP.set("wikipedia.org", wikiHandler)
 DOMAIN_HANDLER_MAP.set("mozilla.org", genericHandler)
 DOMAIN_HANDLER_MAP.set("substack.com", genericHandler)
 
--- src/site-handlers/wiki-handler.ts
+++ src/site-handlers/wiki-handler.ts
@@ -1,9 +1,8 @@
-export function wikiHandler(): Element {
-	// TODO fix bug where it goes to [tools] section
+import { genericHandler } from "./generic-handler"
+
+export function wikiHandler(): Array<Element> {
 	const mainContent = document.querySelector('#mw-content-text')
-	if (!mainContent) {
-		throw new Error('wikiHandler: could not find main content!')
-	}
-	return mainContent
+	const eleArray = genericHandler(mainContent)
+	return eleArray
 }
 
