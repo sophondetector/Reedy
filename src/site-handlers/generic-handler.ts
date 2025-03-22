d67f7b315cad5b5414aac376ecc2dd85de5b97d7
sophondetector <nateisgood@gmail.com>
2025-03-21 20:07:41 -0400

    delete blank line

--- src/site-handlers/generic-handler.ts
+++ src/site-handlers/generic-handler.ts
@@ -2,7 +2,6 @@ export function genericHandler(root: Document | Element | null = null): Array<El
 	if (root === null) {
 		root = document
 	}
-
 	const qsaResult = root.querySelectorAll('h1, h2, h3, h4, h5, p')
 	const arr = Array.from(qsaResult)
 	return arr
