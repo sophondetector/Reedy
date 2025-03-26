2d82640baea24cacccc38ce18e64089a564df605
sophondetector <nateisgood@gmail.com>
2025-03-26 17:31:04 -0400

    Tweak display of options ts; add controls to it

--- src/options.ts
+++ src/options.ts
@@ -1,17 +1,12 @@
 async function getCurrentTab() {
 	let queryOptions = { active: true, lastFocusedWindow: true };
-	// `tab` will either be a `tabs.Tab` instance or `undefined`.
 	let [tab] = await chrome.tabs.query(queryOptions);
 	return tab;
 }
 
-document.querySelector('#reeder-button')!.addEventListener('click', async () => {
-	// get open tab
-
+document.querySelector('#reedy-toggle')!.addEventListener('click', async () => {
 	const tab = await getCurrentTab()
-
 	chrome.tabs.sendMessage(tab.id!, "toggle screen", function() {
 		console.log("sent message to content.ts in open tab")
 	})
 })
-
