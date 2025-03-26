1df68acbaa1e416e6c9630d784b13c090587ca9b
sophondetector <nateisgood@gmail.com>
2025-03-26 15:20:40 -0400

    add TODO to substack handler

--- src/site-handlers/substack-handler.ts
+++ src/site-handlers/substack-handler.ts
@@ -2,6 +2,7 @@ import { genericHandler } from "./generic-handler"
 
 // TODO substack doesn't work on infinite scroll style site
 // TODO substack doesn't work well on logged-in user subscription site
+// TODO fix phantom ranges at the top issue (probably crawler traps)
 export function substackHandler(): Array<Element> {
 	const mainContent = document.querySelector('article')
 	const eleArray = genericHandler(mainContent)
