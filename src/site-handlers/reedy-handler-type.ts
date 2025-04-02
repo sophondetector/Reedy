b7015d2792e982136d7d628c9dce7948ed0a6a94
sophondetector <nateisgood@gmail.com>
2025-04-02 15:40:02 -0400

    add TODO; going to prioritize the fetch event listener for now

--- src/site-handlers/reedy-handler-type.ts
+++ src/site-handlers/reedy-handler-type.ts
@@ -3,3 +3,21 @@ export interface ReedyHandler {
 	getScrollableElement: () => Element | undefined
 }
 
+// TODO experiment with making ^^^ a class
+
+// TODO add generalizable reRangeEvent listener
+
+// TODO use this mutation observer as a base to add a dynamic content observer 
+// lec = "body"
+// // create a new instance of `MutationObserver` named `observer`,
+// // passing it a callback function
+// const observer = new MutationObserver(() => {
+//   console.log("callback that runs when observer is triggered");
+// });
+//
+// // call `observe()`, passing it the element to observe, and the options object
+// observer.observe(document.querySelector(lec), {
+//   subtree: true,
+//   childList: true,
+// });
+//
