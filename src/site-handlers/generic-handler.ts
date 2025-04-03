3ff8c7f08e505acf7c4e0cf1388432612447e641
sophondetector <nateisgood@gmail.com>
2025-04-03 17:29:18 -0400

    add filterContainedElements to generic-handler.ts

--- src/site-handlers/generic-handler.ts
+++ src/site-handlers/generic-handler.ts
@@ -1,6 +1,28 @@
 import { ReedyHandler } from "./reedy-handler-type"
 
-const GENERIC_REEDY_LEC = 'h1, h2, h3, h4, h5, p, ol, ul'
+// TODO is it possible to eliminate the p > a elements??
+const GENERIC_REEDY_LEC = 'h1, h2, h3, h4, h5, p, a, ol, ul'
+
+function filterContainedElements(eleArray: Element[]): Element[] {
+	const backCheckLimit = 5
+	const res: Element[] = []
+	for (let idx = 0; idx < eleArray.length; idx++) {
+		let counter = 0
+		const ele = eleArray[idx]
+		let eleInsideOtherEle = false
+		for (let jdx = idx - 1; jdx > 0 && counter < backCheckLimit; jdx--, counter++) {
+			const otherEle = eleArray[jdx]
+			if (otherEle.contains(ele)) {
+				eleInsideOtherEle = true
+				break
+			}
+		}
+		if (!eleInsideOtherEle) {
+			res.push(ele)
+		}
+	}
+	return res
+}
 
 export function baseElementGetter(root: Element | Document): Array<Element> {
 	const qsaResult = root.querySelectorAll(GENERIC_REEDY_LEC)
@@ -8,7 +30,9 @@ export function baseElementGetter(root: Element | Document): Array<Element> {
 		throw new Error(`baseElementGetter: could not find any reedy elements!`)
 	}
 	const arr = Array.from(qsaResult)
-	return arr
+	// filter out elements which are in other elements
+	// return arr
+	return filterContainedElements(arr)
 }
 
 function genericElementGetter(): Array<Element> {
