218c0aa63389a43d826366de590b96299fca9ce0
sophondetector <nateisgood@gmail.com>
2025-04-30 14:38:00 -0400

    refactor generic handler

--- src/reedy/site-handlers/generic-handler.ts
+++ src/reedy/site-handlers/generic-handler.ts
@@ -1,41 +1,64 @@
 import { ReedyHandler } from "./reedy-handler-type"
 
-// TODO is it possible to eliminate the p > a elements??
-const GENERIC_REEDY_LEC = 'h1, h2, h3, h4, h5, p, a, ol, ul, code'
-
-function filterContainedElements(eleArray: Element[]): Element[] {
-	const backCheckLimit = 5
-	const res: Element[] = []
-	for (let idx = 0; idx < eleArray.length; idx++) {
-		let counter = 0
-		const ele = eleArray[idx]
-		let eleInsideOtherEle = false
-		for (let jdx = idx - 1; jdx > 0 && counter < backCheckLimit; jdx--, counter++) {
-			const otherEle = eleArray[jdx]
-			if (otherEle.contains(ele)) {
-				eleInsideOtherEle = true
-				break
-			}
-		}
-		if (!eleInsideOtherEle) {
-			res.push(ele)
-		}
-	}
-	return res
+//@ts-ignore
+function isSubstack(): boolean {
+	return document.querySelector('link[href="https://substackcdn.com"]') ? true : false
 }
 
-export function baseElementGetter(root: Element | Document): Array<Element> | null {
-	const qsaResult = root.querySelectorAll(GENERIC_REEDY_LEC)
-	if (qsaResult.length < 1) {
-		console.log(`baseElementGetter: could not find any reedy elements!`)
-		return null
+function getParaAndHeaderEles(): Array<Element> | null {
+	const lec = 'p h1 h2 h3 h4 h5'
+	const res = document.querySelectorAll(lec)
+	if (res.length > 0) {
+		console.log("getParaAndHeaderEles: found!")
+		return Array.from(res)
 	}
-	const arr = Array.from(qsaResult)
-	return filterContainedElements(arr)
+	return null
 }
 
-function genericElementGetter(): Array<Element> | null {
-	return baseElementGetter(document)
+function getArticleEle(): HTMLElement | null {
+	const article = document.querySelector('article')
+	if (article) {
+		console.log("getArticleEle: found!")
+		return article
+	}
+	return null
+}
+
+function getBodyChildWithMostText(): Element | null {
+	const candidates = document.querySelectorAll('body > *')
+	let winner;
+	let maxLen = 0;
+	for (const cand of candidates) {
+		//@ts-ignore
+		if (cand.innerText && cand.innerText.length > maxLen) {
+			winner = cand
+			//@ts-ignore
+			maxLen = cand.innerText.length
+		}
+	}
+
+	if (winner) {
+		console.log("getBodyChildWithMostText: found!")
+		return winner
+	}
+
+	return null
+}
+
+export function genericElementGetter(): Array<Element> | null {
+	let res: Element | Array<Element> | null = null;
+
+	res = getParaAndHeaderEles()
+	if (res) return res
+
+	res = getArticleEle()
+	if (res) return [res]
+
+	res = getBodyChildWithMostText()
+	if (res) return [res]
+
+	return null
+
 }
 
 export const genericHandler: ReedyHandler = {
